efe4b6270a6e87cc06b8b1d25ef7fbd798a596e5
lucas.costamanha <lcostamanha@gmail.com>
2023-05-05 10:45:04 -0300

    kkkkkkkk

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -29,3 +29,12 @@ resource "aws_autoscaling_group" "example" {
 
 
 100 - (avg by (instance) (irate(node_filesystem_free_bytes{mountpoint="/"}[5m])) * 100) / (avg by (instance) (irate(node_filesystem_size_bytes{mountpoint="/"}[5m])) * 100)
+
+
+Para exportar dados do DynamoDB para o S3 usando o AWS CLI, você pode usar o seguinte comando:
+
+aws dynamodb scan --table-name <table-name> --region <region> --output json | aws s3 cp - s3://<bucket-name>/<file-name>.json
+
+Substitua <table-name> pelo nome da tabela do DynamoDB que você deseja exportar, <region> pela região em que a tabela está localizada, <bucket-name> pelo nome do bucket S3 para onde você deseja exportar os dados e <file-name> pelo nome do arquivo de saída. Certifique-se de ter as permissões necessárias para acessar a tabela do DynamoDB e o bucket S3.
+
+Espero que isso ajude! Se você tiver alguma dúvida ou precisar de mais ajuda, é só falar.
