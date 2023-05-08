4b2caed5b3fc7bda709c7bc1c210d3d49440be5d
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-05-08 15:22:31 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -29,22 +29,69 @@ resource "aws_autoscaling_group" "example" {
 
 
 ############################################################################################################
-
-# Importar o módulo unittest
+# Importar os módulos necessários
 import unittest
+import boto3
+import json
+from moto import mock_dynamodb2, mock_s3
+from lambda_function import lambda_handler
 
 # Definir uma classe que herda de unittest.TestCase
-class TestGenerico(unittest.TestCase):
+class TestLambda(unittest.TestCase):
+
+    # Definir um método setUp que é executado antes de cada teste
+    def setUp(self):
+        # Criar um mock do DynamoDB e do S3
+        self.mock_dynamodb = mock_dynamodb2()
+        self.mock_s3 = mock_s3()
+        self.mock_dynamodb.start()
+        self.mock_s3.start()
+
+        # Criar uma tabela no DynamoDB com os mesmos atributos da tabela real
+        self.dynamodb = boto3.resource('dynamodb')
+        self.table = self.dynamodb.create_table(
+            TableName='tbes2004_web_rgto_crdl',
+            KeySchema=[
+                {
+                    'AttributeName': 'id',
+                    'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'id',
+                    'AttributeType': 'S'
+                }
+            ]
+        )
+
+        # Inserir alguns dados na tabela
+        self.table.put_item(Item={'id': '1', 'nome': 'João', 'idade': 25})
+        self.table.put_item(Item={'id': '2', 'nome': 'Maria', 'idade': 30})
+
+        # Criar um bucket no S3 com o mesmo nome do bucket real
+        self.s3 = boto3.client('s3')
+        self.s3.create_bucket(Bucket='teste-fido')
+
+    # Definir um método tearDown que é executado depois de cada teste
+    def tearDown(self):
+        # Parar o mock do DynamoDB e do S3
+        self.mock_dynamodb.stop()
+        self.mock_s3.stop()
 
     # Definir um método que começa com test_
-    def test_soma(self):
-        # Usar self.assertEqual para verificar se a soma de dois números é igual ao esperado
-        self.assertEqual(2 + 3, 5)
+    def test_lambda_handler(self):
+        # Chamar a função lambda com um evento e um contexto vazios
+        result = lambda_handler({}, {})
 
-    # Definir outro método que começa com test_
-    def test_subtracao(self):
-        # Usar self.assertEqual para verificar se a subtração de dois números é igual ao esperado
-        self.assertEqual(5 - 3, 2)
+        # Verificar se o resultado tem o status code 200 e a mensagem esperada
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['body'], json.dumps('Dados salvos com sucesso no S3!'))
+
+        # Verificar se os dados foram salvos no S3 corretamente
+        response = self.s3.get_object(Bucket='teste-fido', Key='tbes2004_web_rgto_crdl')
+        data = json.loads(response['Body'].read())
+        self.assertEqual(data, [{'id': '1', 'nome': 'João', 'idade': 25}, {'id': '2', 'nome': 'Maria', 'idade': 30}])
 
 # Executar os testes se o arquivo for executado como um script
 if __name__ == "__main__":
