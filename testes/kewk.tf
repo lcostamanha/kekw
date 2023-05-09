71a7bda516722129439a3c921f0a3d9e3efac184
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-05-09 12:29:25 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,30 +1,8 @@
-resource "aws_autoscaling_group" "example" {
-  name                 = "example-asg"
-  launch_configuration = aws_launch_configuration.example.id
-  min_size             = 1
-  max_size             = 3
-
-  # Use spot instances for the workers
-  mixed_instances_policy {
-    instances_distribution {
-      on_demand_allocation_strategy = "prioritized"
-      on_demand_base_capacity       = 0
-      spot_allocation_strategy      = "capacity-optimized"
-    }
-
-    launch_template {
-      id      = aws_launch_template.example.id
-      version = "$Latest"
-    }
-  }
-
-  # Use a warm pool for the workers
-  warm_pool {
-    min_size     = 1
-    max_size     = 2
-    pool_state   = "stopped"
-    purge_policy = "OldestInstance"
-  }
+{
+   "TableName": "DynamoDB_Table_name",
+   "s3_bucket": "s3_bucket_name",
+   "s3_object": "s3_object_name",
+   "filename": "output.json"
 }
 
 
