ebc12e10e6ebe0bd81182755578918c67d7841d4
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-05-09 13:29:52 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -62,7 +62,65 @@ s3.upload_file(csv_file_name, s3_bucket_name, csv_file_name)
 Esse evento simula a criação de um novo objeto no bucket seu_bucket com o nome seu_arquivo.csv. 
 Quando esse evento é disparado, ele aciona a execução do lambda que lê a tabela sua_tabela do DynamoDB, 
 escreve seus dados em um arquivo CSV e o carrega no bucket S3 seu_bucket com o nome seu_arquivo.csv.
+            
+        
+        
+        
+        
+        
+        
+        
+        
+  
 
 
 
 
+import json
+import traceback
+import boto3
+
+def lambda_handler(event, context):
+    """
+    Export Dynamodb to s3 (JSON)
+    """
+
+    statusCode = 200
+    statusMessage = 'Success'
+
+    try:
+        # parse the payload
+        tableName = event['tableName']
+        s3_bucket = event['s3_bucket']
+        s3_object = event['s3_object']
+        filename = event['filename']
+
+        # scan the dynamodb
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(tableName)
+
+        response = table.scan()
+        data = response['Items']
+
+        # maximum data set limit is 1MB
+        # so we need to have this additional step
+        while 'LastEvaluatedKey' in response:
+            response = dynamodb.scan(
+                TableName=tableName,
+                Select='ALL_ATTRIBUTES',
+                ExclusiveStartKey=response['LastEvaluatedKey'])
+
+            data.extend(response['Items'])
+            
+        # export JSON to s3 bucket
+        s3 = boto3.resource('s3')
+        s3.Object(s3_object, s3_object + filename).put(Body=json.dumps(data))
+
+        except Exception as e:
+            statusCode = 400
+            statusMessage =  traceback.format_exc()
+
+        return {
+            "statusCode": statusCode,
+            "status": statusMessage
+        }
