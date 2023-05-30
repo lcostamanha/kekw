da00156232946893a0ddabfa3ccfc9daf9108b98
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-05-30 16:09:22 -0300

    Update s3.tf

--- testes/s3.tf
+++ testes/s3.tf
@@ -1,61 +1,40 @@
-##################### bucket os-prod-application-artifacts #####################
+provider "aws" {
+  region = "us-east-1"  # Substitua pela região desejada
+}
 
-resource "aws_s3_bucket" "os-prod-application-artifacts" {
-  bucket = "os-prod-application-artifacts"
-  acl = "private"
-  versioning {
-    enabled = false
+resource "aws_s3_bucket" "example" {
+  bucket = "meu-bucket-s3"
+  acl    = "private"
+}
+
+resource "aws_lambda_function" "example" {
+  function_name = "meu-lambda-function"
+  # Configurações do Lambda
+
+  # ...
+
+  # Adicione a permissão do Lambda para acessar o bucket S3
+  provisioned_concurrency_config {
+    function_name    = aws_lambda_function.example.function_name
+    provisioned_concurrent_executions = 1
   }
+}
 
-  tags = {
-    Name = "os-prod-application-artifacts"
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    sid       = "AllowLambdaAccess"
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:PutObject"]
+    resources = ["arn:aws:s3:::meu-bucket-s3/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_lambda_function.example.arn]
+    }
   }
-
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access_block-3" {
-  bucket = aws_s3_bucket.os-prod-application-artifacts.id
-
-  # Block new public ACLs and uploading public objects
-  block_public_acls = true
-
-  # Retroactively remove public access granted through public ACLs
-  ignore_public_acls = true
-
-  # Block new public bucket policies
-  block_public_policy = true
-
-  # Retroactivley block public and cross-account access if bucket has public policies
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_policy" "os-prod-application-artifacts" {
-  bucket = aws_s3_bucket.os-prod-application-artifacts.id
-
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "DelegateS3Access",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": [
-                    "arn:aws:iam::166607387349:role/role-infraestrutura",
-                    "arn:aws:iam::166607387349:role/terraform-os-dev-jenkins"
-                ]
-            },
-            "Action": [
-                "s3:ListBucket",
-                "s3:PutObject",
-                "s3:PutObjectAcl"
-            ],
-            "Resource": [
-                "arn:aws:s3:::os-prod-application-artifacts/*",
-                "arn:aws:s3:::os-prod-application-artifacts"
-            ]
-        }
-    ]
-}
-POLICY
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
