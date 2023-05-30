56c42ebef643ac20aeda6f2da48c1c0b24815134
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-05-30 15:07:17 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,101 +1,22 @@
-import boto3
-import json
-
-def lambda_handler(event, context):
-    # Configurar o cliente do DynamoDB
-    dynamodb = boto3.client('dynamodb')
-    
-    # Configurar o cliente do S3
-    s3 = boto3.client('s3')
-    
-    # Nome da tabela do DynamoDB
-    table_name = 'NomeDaTabela'
-    
-    try:
-        # Obter todos os itens da tabela do DynamoDB
-        response = dynamodb.scan(TableName=table_name)
-        items = response['Items']
-        
-        # Converter os itens para o formato JSON
-        json_data = json.dumps(items)
-        
-        # Nome do bucket do S3
-        bucket_name = 'NomeDoBucket'
-        
-        # Nome do arquivo a ser salvo no S3
-        file_name = 'dados.json'
-        
-        # Gravar os dados no S3
-        s3.put_object(Body=json_data, Bucket=bucket_name, Key=file_name)
-        
-        return {
-            'statusCode': 200,
-            'body': 'Dados salvos com sucesso no S3.'
-        }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': str(e)
-        }
-
-    
-    
-    
-   {
-  "key": "value"
+provider "aws" {
+  region = "us-east-1"  # Substitua pela região desejada
 }
 
-    
-    
-    
-    
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "DynamoDBAccess",
-            "Effect": "Allow",
-            "Action": [
-                "dynamodb:Scan"
-            ],
-            "Resource": [
-                "arn:aws:dynamodb:região:ID-da-conta:dynamodb:Tabela"
-            ]
-        },
-        {
-            "Sid": "S3Access",
-            "Effect": "Allow",
-            "Action": [
-                "s3:PutObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::NomeDoBucket/*"
-            ]
-        }
-    ]
-}
-    
-    
-    
-    
-    
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "arn:aws:iam::ID-da-conta-do-Lambda:role/NomeDaFuncaoDoLambda"
-            },
-            "Action": [
-                "s3:PutObject",
-                "s3:PutObjectAcl"
-            ],
-            "Resource": [
-                "arn:aws:s3:::NomeDoBucket/*"
-            ]
-        }
-    ]
-}
+resource "aws_security_group" "example" {
+  name        = "meu-security-group"
+  description = "Grupo de segurança com regra de saída para HTTPS"
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
