ed7ed27962e31ee371d07c9ef840a0a1165d9e24
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-05-30 15:10:35 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -6,17 +6,10 @@ resource "aws_security_group" "example" {
   name        = "meu-security-group"
   description = "Grupo de segurança com regra de saída para HTTPS"
 
-  ingress {
+  egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
