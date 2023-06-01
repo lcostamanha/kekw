ef693952e5db66e701a5cf659c195fac86a3b6fe
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-01 13:39:46 -0300

    Update s3.tf

--- testes/s3.tf
+++ testes/s3.tf
@@ -1,24 +1,41 @@
-provider "aws" {
-  region = "us-east-1"  # Substitua pela região desejada
-}
+import boto3
+import json
+import datetime
 
-resource "aws_security_group" "example" {
-  name        = "meu-security-group"
-  description = "Security Group com saída liberada para HTTPS"
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Permite todo tráfego de saída"
-  }
-
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Permite tráfego de saída na porta HTTPS (443)"
-  }
-}
+def lambda_handler(event, context):
+    # Configuração do cliente do DynamoDB
+    dynamodb = boto3.client('dynamodb')
+    
+    # Configuração do cliente do S3
+    s3 = boto3.client('s3')
+    
+    # Nome da tabela do DynamoDB
+    table_name = 'NomeDaTabela'
+    
+    # Nome do bucket do S3
+    bucket_name = 'NomeDoBucket'
+    
+    # Obtendo a data atual
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    
+    try:
+        # Obtendo os dados da tabela do DynamoDB
+        response = dynamodb.scan(TableName=table_name)
+        items = response['Items']
+        
+        # Convertendo os itens para um formato JSON
+        json_data = json.dumps(items)
+        
+        # Salvando os dados no S3 com um prefixo de data
+        s3.put_object(Body=json_data, Bucket=bucket_name, Key=f'{current_date}/dados.json')
+        
+        return {
+            'statusCode': 200,
+            'body': 'Dados salvos no S3 com sucesso.'
+        }
+    
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Erro ao salvar os dados no S3: {str(e)}'
+        }
