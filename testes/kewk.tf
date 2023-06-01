6784c6c36cfd4b3bb7dde1e1028bbf820059ff54
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-01 10:04:04 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,15 +1,37 @@
-provider "aws" {
-  region = "us-east-1"  # Substitua pela região desejada
-}
+import boto3
+import json
 
-resource "aws_security_group" "example" {
-  name        = "meu-security-group"
-  description = "Grupo de segurança com regra de saída para HTTPS"
-
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+def lambda_handler(event, context):
+    # Configuração do cliente do DynamoDB
+    dynamodb = boto3.client('dynamodb')
+    
+    # Configuração do cliente do S3
+    s3 = boto3.client('s3')
+    
+    # Nome da tabela do DynamoDB
+    table_name = 'NomeDaTabela'
+    
+    # Nome do bucket do S3
+    bucket_name = 'NomeDoBucket'
+    
+    try:
+        # Obtendo os dados da tabela do DynamoDB
+        response = dynamodb.scan(TableName=table_name)
+        items = response['Items']
+        
+        # Convertendo os itens para um formato JSON
+        json_data = json.dumps(items)
+        
+        # Salvando os dados no S3
+        s3.put_object(Body=json_data, Bucket=bucket_name, Key='nome_do_arquivo.json')
+        
+        return {
+            'statusCode': 200,
+            'body': 'Dados salvos no S3 com sucesso.'
+        }
+    
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Erro ao salvar os dados no S3: {str(e)}'
+        }
