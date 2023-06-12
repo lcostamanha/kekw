1a5646e8ac09ff374568ab63394fb1719dd4181d
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-12 17:54:26 -0300

    Update tf.tf

--- testes/tf.tf
+++ testes/tf.tf
@@ -1,4 +1,1 @@
-sum(rate(http_server_requests_seconds_count{container_name="container-customeriam-webauthnservice", status="success"}[5m])) by (status)
-
-
-+ sum(rate(http_server_requests_seconds_count{container_name="container-customeriam-webauthnservice", status="error"}[5m])) by (status)
+sum(http_server_requests_seconds_count{container_name="container-customeriam-webauthnservice", status="success"}) by (status)
