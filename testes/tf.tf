b2fabbd327bd3c9f8d641ccf1c261bd7e231a9e7
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-14 18:10:27 -0300

    Update tf.tf

--- testes/tf.tf
+++ testes/tf.tf
@@ -1,3 +1,6 @@
 sum by (status_code) (http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", uri!="/actuator/prometheus", status_code=~"2.."})
 
 sum by (status_code) (http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", uri!="/actuator/prometheus", status_code!~"2.."})
+
+
+sum(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"})
