9bcb16dd60698c084b8a1f66288463110800f6ea
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-15 11:03:55 -0300

    Update tf.tf

--- testes/tf.tf
+++ testes/tf.tf
@@ -3,6 +3,7 @@ coalesce(sum(irate(http_server_requests_seconds_count{task_group="service:servic
 
 if(sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])) > 0, sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])) * 100 / sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"}[5m])), 0)
 
+sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])) * 100 / (sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"}[5m])) + unless(sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])) > 0, sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])), 0))
 
 
 fields @timestamp, campo_nulo
