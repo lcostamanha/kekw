d3ace3887d3321b10c37b65bb6f8feac5ebea6cc
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-15 11:33:53 -0300

    Update tf.tf

--- testes/tf.tf
+++ testes/tf.tf
@@ -2,3 +2,6 @@ sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webaut
 
 
 100 - sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus|/"}[5m])) / sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"}[5m])) * 100
+
+
+if(sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus|/"}[5m])) > 0, 100 - sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus|/"}[5m])) / sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"}[5m])) * 100, 0)
