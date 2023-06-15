aaaa18f187442c42d566a77702be8d53591e0b3c
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-15 15:40:40 -0300

    Update tf.tf

--- testes/tf.tf
+++ testes/tf.tf
@@ -3,3 +3,6 @@ sum by (status)(rate(http_server_requests_seconds_count{task_name="family-custom
 
 
 sum by (status)(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status="error", uri="/api/v1/preregister"}[5m])) / ignoring(status) group_left sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preregister"}[5m])) * 100
+
+
+histogram_quantile(0.95, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preregister"}[5m])) by (le))
