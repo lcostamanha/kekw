c4cccead9d870b9bb07fde54dad58cf4dc5eae5d
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-15 10:56:38 -0300

    Update tf.tf

--- testes/tf.tf
+++ testes/tf.tf
@@ -1,27 +1,12 @@
-sum by (status_code) (http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", uri!="/actuator/prometheus", status_code=~"2.."})
-
-sum by (status_code) (http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", uri!="/actuator/prometheus", status_code!~"2.."})
+coalesce(sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])) * 100 / sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"}[5m])), 0)
 
 
-sum(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"})
+if(sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])) > 0, sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])) * 100 / sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"}[5m])), 0)
 
 
-sum by (status_code) (
-  http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"}
-) / ignoring(status_code) group_left sum by (1) (
-  http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"}
-) * 100
 
-
-sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])) * 100 / sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"}[5m]))
-
----
-
-sus
-
-sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])) * 100 / sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"}[5m]))
-
-
-error
-
-100 - (sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice", status_code=~"2.*"}[5m])) * 100 / sum(irate(http_server_requests_seconds_count{task_group="service:service-customeriam-webauthnservice"}[5m])))
+fields @timestamp, campo_nulo
+| filter campo_nulo = "null"
+| stats count(*) by campo_nulo
+| fields - campo_nulo
+| fields @timestamp, campo_nulo="0"
