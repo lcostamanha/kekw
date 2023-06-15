d9e90ee5f0088d91eb8e19a84900554359da9aec
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-15 15:55:01 -0300

    Update tf.tf

--- testes/tf.tf
+++ testes/tf.tf
@@ -4,8 +4,17 @@ sum by (status)(rate(http_server_requests_seconds_count{task_name="family-custom
 
 sum by (status)(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status="error", uri="/api/v1/preregister"}[5m])) / ignoring(status) group_left sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preregister"}[5m])) * 100
 
+--------------------------------------
+
 
 histogram_quantile(0.95, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preregister"}[5m])) by (le))
 
 
 histogram_quantile(0.95, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus|/"}[5m])) by (le))
+
+-----------------------
+
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"}[5m]))
+
+
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"}[1m])) by (uri)
