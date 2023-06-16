5679b69809b4ae3ecdd8b8083ac64172c1b93321
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-16 15:44:05 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,37 +1,12 @@
-import boto3
-import json
+histogram_quantile(0.95, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus"}[5m])) by (le))
 
-def lambda_handler(event, context):
-    # Configuração do cliente do DynamoDB
-    dynamodb = boto3.client('dynamodb')
-    
-    # Configuração do cliente do S3
-    s3 = boto3.client('s3')
-    
-    # Nome da tabela do DynamoDB
-    table_name = 'NomeDaTabela'
-    
-    # Nome do bucket do S3
-    bucket_name = 'NomeDoBucket'
-    
-    try:
-        # Obtendo os dados da tabela do DynamoDB
-        response = dynamodb.scan(TableName=table_name)
-        items = response['Items']
-        
-        # Convertendo os itens para um formato JSON
-        json_data = json.dumps(items)
-        
-        # Salvando os dados no S3
-        s3.put_object(Body=json_data, Bucket=bucket_name, Key='nome_do_arquivo.json')
-        
-        return {
-            'statusCode': 200,
-            'body': 'Dados salvos no S3 com sucesso.'
-        }
-    
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': f'Erro ao salvar os dados no S3: {str(e)}'
-        }
+
+
+
+
+
+
+
+
+
+histogram_quantile(0.90, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus"}[5m])) by (le))
