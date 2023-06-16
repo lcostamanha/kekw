bd0142cfb907c927bf6a69fd51ef0924ff916bbc
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-16 15:46:28 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,12 +1,7 @@
 histogram_quantile(0.95, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus"}[5m])) by (le))
 
 
-
-
-
-
-
-
-
-
 histogram_quantile(0.90, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus"}[5m])) by (le))
+
+
+histogram_quantile(0.50, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus"}[5m])) by (le))
