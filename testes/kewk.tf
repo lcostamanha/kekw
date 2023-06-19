e1d81c457ee14c8d369c03c354334f6c20d70451
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-19 14:12:03 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,8 +1,10 @@
-sum by (status) (rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2..", uri="/api/v1/preauthenticate"}[5m]))
+sum by (status) (rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2[0-9]{2}", status!="204", uri="/api/v1/preauthenticate"}[5m]))
 / ignoring(status) group_left sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"}[5m]))
 * 100
 
 
+sum by (status) (rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status="204", uri="/api/v1/preauthenticate"}[5m]))
++
 sum by (status) (rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status="error", uri="/api/v1/preauthenticate"}[5m]))
 / ignoring(status) group_left sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"}[5m]))
 * 100
