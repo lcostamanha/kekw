813bc5725c985c93ffad7f46b95084faf60ee48d
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-19 15:13:36 -0300

    Update s3.tf

--- testes/s3.tf
+++ testes/s3.tf
@@ -7,3 +7,15 @@ sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webaut
 
 
 sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus", uri!="/"} offset $__interval)
+
+
+------
+
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus", uri!="/", status=~"2.."}[5m]))
+/ sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus", uri!="/" }[5m]))
+* 100
+
+
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus", uri!="/", status!~"2.."}[5m]))
+/ sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus", uri!="/" }[5m]))
+* 100
