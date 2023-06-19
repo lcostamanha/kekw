31f8c9bafb5d97d58cee75a5ebcef0f65b218edc
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-19 14:09:50 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,17 +1,8 @@
-avg(http_server_request_duration_seconds{task_name="family-customeriam-webauthnservice-newvpc", uri!="/api/v1/preregister"})
+sum by (status) (rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2..", uri="/api/v1/preauthenticate"}[5m]))
+/ ignoring(status) group_left sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"}[5m]))
+* 100
 
 
-
-------------------------------------------------------------------------------------------------
-
-histogram_quantile(0.95, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus"}[5m])) by (le))
-
-
-histogram_quantile(0.90, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus"}[5m])) by (le))
-
-
-histogram_quantile(0.50, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus"}[5m])) by (le))
-
------------------------------------
-
-avg(http_request_duration_seconds{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus"})
+sum by (status) (rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status="error", uri="/api/v1/preauthenticate"}[5m]))
+/ ignoring(status) group_left sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"}[5m]))
+* 100
