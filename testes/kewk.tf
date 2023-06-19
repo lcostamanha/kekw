ba709ae85aa67075ff230aacaa1328411ed568df
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-19 14:37:14 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -21,3 +21,8 @@ porcetagem de errros
 sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status="error", uri="/api/v1/preauthenticate"}[5m]))
 / sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"}[5m]))
 * 100
+
+
+Latencia
+
+histogram_quantile(0.95, sum(rate(http_server_request_duration_seconds_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"}[5m])) by (le)) * 1000
