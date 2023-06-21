1f05e48676b9a3b105f97d85f57648f9c32e3178
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-21 19:28:00 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,1 +1,3 @@
-sum by (method) (http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"})
+sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status_code=~"4..|5.."})
+
+rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status_code=~"4..|5.."}[1m])
