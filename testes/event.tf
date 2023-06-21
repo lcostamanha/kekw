2953f957dcf52744de495a5cb4e6570319fc710d
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-21 15:55:27 -0300

    Update event.tf

--- testes/event.tf
+++ testes/event.tf
@@ -1,1 +1,3 @@
 clamp_max(sum(http_server_requests_success_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2[0-9]{2}"}) / sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"}) * 100, 100)
+
+100 - clamp_max(sum(http_server_requests_success_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2[0-9]{2}"}) / sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"}) * 100, 100)
