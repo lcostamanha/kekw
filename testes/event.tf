88ce7d695ef168e5e4fff39c458a156d88b10c91
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-21 15:04:49 -0300

    Update event.tf

--- testes/event.tf
+++ testes/event.tf
@@ -1,3 +1,7 @@
 sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2.."}[1m])) / sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"}[1m])) * 100
 
 sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"4..|5..", uri!="/actuator/prometheus"}[1m])) / sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"}[1m])) * 100
+
+
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2.."}[1m])) / sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"}[1m])) * 100 or
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"4..|5..", uri!="/actuator/prometheus"}[1m])) / sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"}[1m])) * 100
