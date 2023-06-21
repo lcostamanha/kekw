198579d04e26d5257e4d5382f348ed41c74f0242
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-21 16:05:39 -0300

    Update event.tf

--- testes/event.tf
+++ testes/event.tf
@@ -1,4 +1,23 @@
+total 
+
+sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"})
+
+por min
+
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"}[1m]))
+
+médio de res
+
+avg(http_server_requests_seconds_sum{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"} / http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"})
+
+% de erro
+
 100 - (
-  sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2[0-9]{2}"}) /
-  sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"})
+  sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate", status=~"2[0-9]{2}"}[1m])) /
+  sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate"}[1m]))
 ) * 100
+
+
+por min
+
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate", status!~"2[0-9]{2}"}[1m]))
