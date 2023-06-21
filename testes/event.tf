8ee7c3afa01d318332ca8a54ccad452e91e1a13e
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-21 14:57:02 -0300

    Update event.tf

--- testes/event.tf
+++ testes/event.tf
@@ -1,14 +1,5 @@
-# Criação da regra do EventBridge
-resource "aws_cloudwatch_event_rule" "example_event_rule" {
-  name        = "my-event-rule"
-  description = "Regra para invocar a função Lambda a cada cinco minutos"
+(sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2..", uri="/api/v1/authenticate"}) / sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"})) * 100
 
-  schedule_expression = "rate(5 minutes)"  # Expressão de agendamento para invocação a cada cinco minutos
-}
 
-# Associação da regra do EventBridge com a função Lambda
-resource "aws_cloudwatch_event_target" "example_event_target" {
-  rule      = aws_cloudwatch_event_rule.example_event_rule.name
-  target_id = aws_lambda_function.example_lambda.function_name
-  arn       = aws_lambda_function.example_lambda.arn
-}
+
+(sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2..", uri="/api/v1/authenticate"}) / sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"})) * 100
