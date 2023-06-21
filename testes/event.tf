21e3a29cb594e8e44ea17f53137b2a420c4d43fd
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-21 14:58:08 -0300

    Update event.tf

--- testes/event.tf
+++ testes/event.tf
@@ -1,5 +1,4 @@
-(sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2..", uri="/api/v1/authenticate"}) / sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"})) * 100
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2..", uri="/api/v1/authenticate"}[1m])) / sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"}[1m])) * 100
 
 
-
-(sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"2..", uri="/api/v1/authenticate"}) / sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"})) * 100
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status=~"4..|5..", status!="404", uri="/api/v1/authenticate"}[1m])) / sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc"}[1m])) * 100
