fa69dcdceb3490e32243eded6e05b0f797001701
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-21 15:45:37 -0300

    Update event.tf

--- testes/event.tf
+++ testes/event.tf
@@ -1,1 +1,3 @@
 ceil(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"}[1m]) * 60)
+
+floor(sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"}[1m])) * 60)
