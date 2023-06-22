3b813a6f4eb5167f6ed6dccdc3bba49a70145897
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-22 11:04:32 -0300

    Update s3.tf

--- testes/s3.tf
+++ testes/s3.tf
@@ -3,3 +3,4 @@ floor(sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-
 
 floor(sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate", uri!="/actuator/prometheus", status=~"4..|5..", status!="404"}[1m])) * 60)
 
+sum by (status_code) (http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate", uri!="/actuator/prometheus"})
