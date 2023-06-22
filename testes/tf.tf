589d42250423550a00b066db1f201b8fd7e64be2
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-22 15:44:35 -0300

    Update tf.tf

--- testes/tf.tf
+++ testes/tf.tf
@@ -1,32 +1,11 @@
-sum by (status)(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status!="error", uri="/api/v1/preregister"}[5m])) / ignoring(status) group_left sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preregister"}[5m])) * 100
+No Prometheus e no Grafana, as funções `increase`, `sum` e `rate` têm finalidades diferentes na manipulação e visualização de métricas. Aqui está uma explicação sobre cada uma delas:
 
+1. `increase`: A função `increase` é usada para calcular a variação ou aumento de uma métrica ao longo do tempo. Ela retorna a diferença entre o valor atual e o valor anterior da métrica dentro de um intervalo de tempo especificado. Essa função é útil para analisar a taxa de crescimento ou mudança em uma métrica ao longo do tempo.
 
+2. `sum`: A função `sum` é utilizada para somar os valores de uma métrica em um determinado período de tempo. Ela permite agregar os valores de várias séries temporais em uma única série, fornecendo uma visão consolidada dos dados. Essa função é útil quando você deseja obter a soma total de uma métrica em vez de observar a variação ao longo do tempo.
 
-sum by (status)(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", status="error", uri="/api/v1/preregister"}[5m])) / ignoring(status) group_left sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preregister"}[5m])) * 100
+3. `rate`: A função `rate` é utilizada para calcular a taxa de variação ou taxa de crescimento de uma métrica em relação ao tempo. Ela fornece a taxa de aumento média da métrica por unidade de tempo (por exemplo, por segundo ou por minuto). Essa função é útil quando você deseja analisar a taxa de mudança de uma métrica em tempo real.
 
---------------------------------------
+No Grafana, essas funções podem ser usadas em combinação com outras opções de visualização, como gráficos de linha, gráficos de barras ou gráficos de pizza, para fornecer insights e análises sobre os dados métricos coletados pelo Prometheus.
 
-
-histogram_quantile(0.95, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preregister"}[5m])) by (le))
-
-
-histogram_quantile(0.95, sum(rate(preregister_histogram_bucket{task_name="family-customeriam-webauthnservice-newvpc", uri!~"/actuator/prometheus|/"}[5m])) by (le))
-
------------------------
-
-sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"}[5m]))
-
-
-sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"}[1m])) by (uri)
-
-
-
-
-sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"}[1m]))
-
-++========================================================
-
-sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"})
-
-
-sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"}[1m])) * 60
+É importante entender a finalidade de cada função e como aplicá-las corretamente em diferentes cenários para obter informações relevantes e úteis a partir das métricas coletadas.
