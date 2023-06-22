add3d0ba39b8699c427fe21a06018b92966c7079
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-22 10:02:53 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,1 +1,1 @@
-rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"}[1m])
+sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri!="/actuator/prometheus"}[1m]))
