cfa3c72907c5e811ac1282a3588532590c92e166
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-22 14:26:51 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,1 +1,3 @@
 floor(sum(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate", status=~"4[0-3][0-9]|4[5-9][0-9]|5[0-9][0-9]", status!="404"}))
+
+floor(sum(rate(http_server_requests_seconds_count{task_name="family-customeriam-webauthnservice-newvpc", uri="/api/v1/preauthenticate", status=~"4[0-3][0-9]|4[5-9][0-9]|5[0-9][0-9]", status!="404"}[1m])) * 60)
