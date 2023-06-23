035ff56735de4bd4c3d4262cbb95a6251f836430
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-23 11:30:14 -0300

    Update tf.tf

--- testes/tf.tf
+++ testes/tf.tf
@@ -12,3 +12,9 @@ filter @message like /"resource_patch":/
 
 filter resource_patch is not null
 | fields resource_patch
+
+filter @message like /"status": "200"/ and @message like /"resource_path": "\/webauthn_service\/v1\/credenciais"/
+| display @message
+
+filter @message like /"status": "200"/ and @message like /"resource_path": "\/webauthn_service\/v1\/credenciais"/ and not @message like /"status": "403"/
+| display @message
