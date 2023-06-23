7c1bb83ae5799dc0b129420a68e2306384c240f0
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-06-23 11:14:07 -0300

    Update tf.tf

--- testes/tf.tf
+++ testes/tf.tf
@@ -4,3 +4,7 @@ filter @message like /"resource_path":"\/webauthn_service\/v1\/pre_authenticacoe
 
 filter @message like /"resource_path":"\/webauthn_service\/v1\/pre_authenticacoes"/
 | fields @message
+
+
+filter @message like /"resource_patch":/
+| display @message
