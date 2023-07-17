b7b9d0a694b3fce895fa597f60c958d2ae7748ce
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-07-17 18:55:57 -0300

    Update s3.tf

--- testes/s3.tf
+++ testes/s3.tf
@@ -3,7 +3,6 @@ import json
 import datetime
 import os
 import pandas as pd
-import fastparquet as fp
 
 def lambda_handler(event, context):
     # Configuração do cliente do DynamoDB
@@ -38,13 +37,9 @@ def lambda_handler(event, context):
             # Cria um DataFrame a partir dos itens
             df = pd.DataFrame(items)
 
-            # Salva o DataFrame em formato Parquet
+            # Salva o DataFrame em formato Parquet diretamente no S3
             file_name = f'{current_date}/fido-export.parquet'
-            with open(f'/tmp/{file_name}', 'wb') as f:
-                fp.write(f, df)
-
-            # Envia o arquivo para o S3
-            s3.upload_file(f'/tmp/{file_name}', bucket_name, file_name)
+            s3.put_object(Body=df.to_parquet(index=False), Bucket=bucket_name, Key=file_name)
 
         return {
             'statusCode': 200,
