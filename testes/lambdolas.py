f2e51f6f7955f512af80b77967db495d43ec96e9
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-07-25 20:31:57 -0300

    Update lambdolas.py

--- testes/lambdolas.py
+++ testes/lambdolas.py
@@ -1,61 +1,63 @@
 import boto3
-from collections import defaultdict
+import datetime
 import pandas as pd
+import os
 
 def flatten_value(value):
     if isinstance(value, dict):
         for k, v in value.items():
             if isinstance(v, dict) and len(v) == 1:
                 v_key, v_value = list(v.items())[0]
-                return {k: v_value}
+                if v_key in ('S', 'B', 'N'):
+                    return {k: v_value}
+                elif v_key == 'L':
+                    return {k: list(map(flatten_value, v_value))}
+                elif v_key == 'M':
+                    return {k: flatten_value(v_value)}
             else:
                 return {k: flatten_value(v)}
     return value
-
-def save_to_parquet(data, bucket_name, file_name):
-    # Cria o DataFrame a partir dos dados
-    df = pd.DataFrame(data)
-
-    # Salva o DataFrame em formato Parquet no S3
-    s3 = boto3.client('s3')
-    s3.put_object(Body=df.to_parquet(), Bucket=bucket_name, Key=file_name)
 
 def lambda_handler(event, context):
     # Configuração do cliente do DynamoDB
     dynamodb = boto3.client('dynamodb')
 
+    # Configuração do cliente do S3
+    s3 = boto3.client('s3')
+
+    # Nome da tabela do DynamoDB
+    table_name = 'tbes2004_web_rgto_crdl'
+
+    # Nome do bucket do S3
+    bucket_name = os.environ['BUCKET_NAME']
+
+    # Obtendo a data atual
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
     try:
-        # Consulta todas as tabelas disponíveis no DynamoDB
-        response = dynamodb.list_tables()
-        tables = response['TableNames']
+        # Obtendo todos os itens da tabela do DynamoDB
+        response = dynamodb.scan(TableName=table_name)
+        items = response['Items']
 
-        # Dicionário para armazenar os resultados
-        results = defaultdict(list)
-
-        # Para cada tabela, consulta todos os itens
-        for table_name in tables:
-            response = dynamodb.scan(TableName=table_name)
-            items = response['Items']
-
-            # Verifica se há mais páginas de resultados
+        # Verifica se há mais páginas de resultados
+        last_evaluated_key = response.get('LastEvaluatedKey')
+        while last_evaluated_key:
+            response = dynamodb.scan(TableName=table_name, ExclusiveStartKey=last_evaluated_key)
+            items.extend(response['Items'])
             last_evaluated_key = response.get('LastEvaluatedKey')
-            while last_evaluated_key:
-                response = dynamodb.scan(TableName=table_name, ExclusiveStartKey=last_evaluated_key)
-                items.extend(response['Items'])
-                last_evaluated_key = response.get('LastEvaluatedKey')
 
-            # Transforma os itens em uma estrutura mais plana
-            transformed_items = [{key: flatten_value(value) for key, value in item.items()} for item in items]
+        # Transforma os itens em uma estrutura mais plana
+        transformed_items = [{key: flatten_value(value) for key, value in item.items()} for item in items]
 
-            # Adiciona os itens transformados ao resultado final
-            for item in transformed_items:
-                for key, value in item.items():
-                    results[key].append(value)
+        # Cria o DataFrame a partir dos itens transformados
+        df = pd.DataFrame(transformed_items)
 
-        # Salva os dados no formato Parquet no S3
-        bucket_name = 'NOME_DO_BUCKET'  # Substitua pelo nome do seu bucket S3
-        file_name = 'dados.parquet'  # Nome do arquivo no S3
-        save_to_parquet(results, bucket_name, file_name)
+        # Salva o DataFrame em formato Parquet no diretório temporário
+        tmp_file_name = f'/tmp/{current_date}.parquet'
+        df.to_parquet(tmp_file_name, compression='GZIP')
+
+        # Envia o arquivo para o S3 (na pasta tb_fido)
+        s3.upload_file(tmp_file_name, bucket_name, f'{folder_name}/{current_date}.parquet')
 
         return {
             'statusCode': 200,
@@ -65,5 +67,5 @@ def lambda_handler(event, context):
     except Exception as e:
         return {
             'statusCode': 500,
-            'body': f'Erro ao consultar os dados no DynamoDB: {str(e)}'
+            'body': f'Erro ao salvar os dados no S3: {str(e)}'
         }
