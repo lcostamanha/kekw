46066fcb81f9e8296edcbd09e5e2d0eea8ba50f3
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-07-26 10:56:37 -0300

    Update lambdolas.py

--- testes/lambdolas.py
+++ testes/lambdolas.py
@@ -3,29 +3,6 @@ import datetime
 import pandas as pd
 import os
 
-def flatten_value(value):
-    if isinstance(value, dict):
-        for k, v in value.items():
-            if isinstance(v, dict) and len(v) == 1:
-                v_key, v_value = list(v.items())[0]
-                if v_key in ('S', 'B', 'N'):
-                    return {k: v_value}
-                elif v_key == 'L':
-                    return {k: list(map(flatten_value, v_value))}
-                elif v_key == 'M':
-                    return {k: flatten_value(v_value)}
-            else:
-                return {k: flatten_value(v)}
-    return value
-
-def transform_items(items):
-    transformed_items = []
-    for item in items:
-        transformed_item = {key: flatten_value(value) for key, value in item.items()}
-        transformed_item = {k: v for key, value in transformed_item.items() for k, v in value.items()}
-        transformed_items.append(transformed_item)
-    return transformed_items
-
 def lambda_handler(event, context):
     # Configuração do cliente do DynamoDB
     dynamodb = boto3.client('dynamodb')
@@ -57,15 +34,14 @@ def lambda_handler(event, context):
             items.extend(response['Items'])
             last_evaluated_key = response.get('LastEvaluatedKey')
 
-        # Transforma os itens em uma estrutura mais plana
-        transformed_items = transform_items(items)
+        # Transforma os itens em um formato mais simples (sem a camada de chave-valor)
+        for item in items:
+            for key, value in item.items():
+                if isinstance(value, dict) and len(value) == 1:
+                    item[key] = list(value.values())[0]
 
         # Cria o DataFrame a partir dos itens transformados
-        df = pd.DataFrame(transformed_items)
-
-        # Transforma os tipos de dados para remover a camada de chave-valor
-        for column in df.columns:
-            df[column] = df[column].apply(lambda x: x[column] if isinstance(x, dict) and column in x else x)
+        df = pd.DataFrame(items)
 
         # Salva o DataFrame em formato Parquet no diretório temporário
         tmp_file_name = f'/tmp/{current_date}.parquet'
