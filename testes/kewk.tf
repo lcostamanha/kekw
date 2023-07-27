353f6830deed4cb52c8b74cee59652b279d8e238
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-07-27 10:32:58 -0300

    Update kewk.tf

--- testes/kewk.tf
+++ testes/kewk.tf
@@ -1,9 +1,42 @@
 import boto3
-import json
 import datetime
-import os
 import pandas as pd
-import fastparquet
+import os
+
+def flatten_value(value):
+    if isinstance(value, dict):
+        # Ignora completamente o campo txt_objt_usua
+        if 'txt_objt_usua' in value:
+            del value['txt_objt_usua']
+
+        # Se o campo txt_objt_chav_pubi existir, tenta extrair o campo description
+        if 'txt_objt_chav_pubi' in value and isinstance(value['txt_objt_chav_pubi'], dict):
+            description_value = value['txt_objt_chav_pubi']
+            for key in ['M', 'nom_idef_mtdo', 'device_properties']:
+                if key in description_value:
+                    description_value = description_value[key]
+                else:
+                    description_value = None
+                    break
+
+            if description_value is not None and isinstance(description_value, list):
+                for item in description_value:
+                    if isinstance(item, dict) and 'description' in item:
+                        value['txt_objt_chav_pubi'] = item['description']
+                        break
+                else:
+                    value['txt_objt_chav_pubi'] = ''
+
+        if len(value) == 1:
+            v_key, v_value = list(value.items())[0]
+            if v_key in ('S', 'B', 'N'):
+                return v_value
+            elif v_key == 'M':
+                return flatten_value(v_value)
+        return {k: flatten_value(v) for k, v in value.items() if k != 'L'}  # Ignora os campos que são 'L'
+    elif isinstance(value, list):
+        return [flatten_value(v) for v in value]
+    return value
 
 def lambda_handler(event, context):
     # Configuração do cliente do DynamoDB
@@ -17,6 +50,9 @@ def lambda_handler(event, context):
 
     # Nome do bucket do S3
     bucket_name = os.environ['BUCKET_NAME']
+
+    # Nome da pasta no bucket do S3
+    folder_name = 'tb_fido'
 
     # Obtendo a data atual
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -33,17 +69,18 @@ def lambda_handler(event, context):
             items.extend(response['Items'])
             last_evaluated_key = response.get('LastEvaluatedKey')
 
-        # Verifica se há itens para salvar
-        if items:
-            # Cria um DataFrame a partir dos itens
-            df = pd.DataFrame(items)
+        # Transforma os itens em um formato mais simples (sem a camada de chave-valor)
+        transformed_items = [flatten_value(item) for item in items]
 
-            # Salva o DataFrame em formato Parquet
-            file_name = f'{current_date}/fido-export.parquet'
-            fastparquet.write(file_name, df, compression='GZIP')
+        # Cria o DataFrame a partir dos itens transformados
+        df = pd.DataFrame(transformed_items)
 
-            # Envia o arquivo para o S3
-            s3.upload_file(file_name, bucket_name, file_name)
+        # Salva o DataFrame em formato Parquet no diretório temporário
+        tmp_file_name = f'/tmp/{current_date}.parquet'
+        df.to_parquet(tmp_file_name, compression='GZIP')
+
+        # Envia o arquivo para o S3 (na pasta tb_fido)
+        s3.upload_file(tmp_file_name, bucket_name, f'{folder_name}/{current_date}.parquet')
 
         return {
             'statusCode': 200,
