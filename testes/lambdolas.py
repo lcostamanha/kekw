98f6bfcf3c2b52cdbf8a85e742505d8de51a0fef
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-02 01:49:28 -0300

    Update lambdolas.py

--- testes/lambdolas.py
+++ testes/lambdolas.py
@@ -3,31 +3,6 @@ import datetime
 import pandas as pd
 import os
 
-def flatten_value(value):
-    if isinstance(value, dict):
-        if 'txt_objt_usua' in value:
-            del value['txt_objt_usua']
-
-        if 'txt_objt_chav_pubi' in value:
-            value['txt_objt_chav_pubi'] = get_description(value['txt_objt_chav_pubi'])
-
-        if len(value) == 1:
-            v_key, v_value = list(value.items())[0]
-            if v_key in ('S', 'B', 'N'):
-                return v_value
-            elif v_key == 'M':
-                return flatten_value(v_value)
-        return {k: flatten_value(v) for k, v in value.items() if k != 'L'}
-    elif isinstance(value, list):
-        return [flatten_value(v) for v in value]
-    return value
-
-def get_description(obj):
-    try:
-        return obj['M']['nom_idef_mtdo']['M']['device_properties']['L'][0]['M']['description']['S']
-    except (KeyError, IndexError, TypeError):
-        return ''
-
 def lambda_handler(event, context):
     dynamodb = boto3.client('dynamodb')
     s3 = boto3.client('s3')
@@ -48,9 +23,7 @@ def lambda_handler(event, context):
                 items.extend(response['Items'])
                 last_evaluated_key = response.get('LastEvaluatedKey')
 
-        transformed_items = [flatten_value(item) for item in items]
-
-        df = pd.DataFrame(transformed_items)
+        df = pd.DataFrame(items)
 
         tmp_file_name = f'/tmp/{current_date}.parquet'
         df.to_parquet(tmp_file_name, compression='GZIP')
