2652fee5c42d1171cd853fd43487e9533182424b
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-03 12:03:54 -0300

    Update lambdolas.py

--- testes/lambdolas.py
+++ testes/lambdolas.py
@@ -1,47 +1,56 @@
 import sys
-import boto3
-from awsglue.transforms import ApplyMapping
+import datetime
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-from awsglue.job import Job
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME"])
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args["JOB_NAME"], args)
+ARG_TABLE_NAME = "table_name"
+ARG_READ_PERCENT = "read_percentage"
+ARG_OUTPUT = "output_prefix"
+ARG_FORMAT = "output_format"
 
-bucket_name = 'extraction-fido-dev'
-table_name = 'tbes2004_web_rgto_crdl'
+PARTITION = "snapshot_timestamp"
 
-# Use the DynamoDB connector for Apache Spark
-dynamodb = boto3.resource('dynamodb', region_name='sa-east-1') 
-table = dynamodb.Table(table_name)
-items = table.scan()['Items']
-
-# Convert the items to a DataFrame
-df = spark.createDataFrame(items)
-
-# Print the schema of the DataFrame
-df.printSchema()
-
-# Apply the necessary transformations
-# Change this mappings to fit your table schema
-df_transformed = ApplyMapping.apply(
-    frame = df,
-    mappings = [
-        ("cod_perf_aces", "int", "cod_perf_aces", "int"),
-        ("nom_perf_aces", "string", "nom_perf_aces", "string"),
-        ("des_perf_aces", "string", "des_perf_aces", "string")
-    ]
+args = getResolvedOptions(sys.argv,
+  [
+    'JOB_NAME',
+    ARG_TABLE_NAME,
+    ARG_READ_PERCENT,
+    ARG_OUTPUT,
+    ARG_FORMAT
+  ]
 )
 
-# Print the schema of the transformed DataFrame
-df_transformed.printSchema()
+table_name = args[ARG_TABLE_NAME]
+read = args[ARG_READ_PERCENT]
+output_prefix = args[ARG_OUTPUT]
+fmt = args[ARG_FORMAT]
 
-# Write the DataFrame to S3 as a parquet file
-df_transformed.write.parquet(f"s3://{bucket_name}/data/output/")
+print("Table name:", table_name)
+print("Read percentage:", read)
+print("Output prefix:", output_prefix)
+print("Format:", fmt)
 
-job.commit()
+date_str = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M')
+output = "%s/%s=%s" % (output_prefix, PARTITION, date_str)
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+
+table = glueContext.create_dynamic_frame.from_options(
+  "dynamodb",
+  connection_options={
+    "dynamodb.input.tableName": "tbes2004_web_rgto_crdl",  # Your DynamoDB Table Name
+    "dynamodb.throughput.read.percent": "0.5"  # Read capacity percentage
+  }
+)
+
+glueContext.write_dynamic_frame.from_options(
+  frame=table,
+  connection_type="s3",
+  connection_options={
+    "path": "s3://extraction-fido-dev/"  # Your S3 Bucket Name
+  },
+  format="parquet",
+  transformation_ctx="datasink"
+)
