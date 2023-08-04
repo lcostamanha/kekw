fb1466332a8821a8a7a09f8d0aa8bff67429d6ff
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-04 11:28:24 -0300

    Update lambdolas.py

--- testes/lambdolas.py
+++ testes/lambdolas.py
@@ -1,38 +1,32 @@
 import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
-
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Define your DynamoDB table name
-table_name = "tbes2004_web_rgto_crdl"
-# Define your output path
-output_path = "s3://extraction-fido-dev/glue/"
-
 # Reading data from DynamoDB
-dynamodb_df = glueContext.create_dynamic_frame.from_options(
+ddb_frame = glueContext.create_dynamic_frame.from_options(
     connection_type="dynamodb",
     connection_options={
-        "dynamodb.input.tableName": table_name,
-        "dynamodb.throughput.read.percent": "1.0"
-    }
+        "dynamodb.input.tableName": "tbes2004_web_rgto_crdl",
+        "dynamodb.throughput.read.percent": "0.5",
+        "dynamodb.splits": "100",
+    },
 )
 
-# Write the data to S3 in parquet format
+# Writing data to S3
 glueContext.write_dynamic_frame.from_options(
-    frame=dynamodb_df,
+    frame=ddb_frame,
     connection_type="s3",
-    connection_options={"path": output_path},
-    format="parquet"
+    connection_options={"path": "s3://extraction-fido-dev/glue/"},
+    format="parquet",
 )
 
 job.commit()
