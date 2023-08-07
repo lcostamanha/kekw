77ca3aa2d4381ce4251ce8d70ba0ed3446e0137e
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-07 14:54:34 -0300

    Update glue.py

--- testes/glue.py
+++ testes/glue.py
@@ -1,34 +1,59 @@
 import sys
+from awsglue.transforms import ApplyMapping
+from awsglue.utils import getResolvedOptions
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.context import SparkContext
+from pyspark.sql.types import StructField, StructType, StringType
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
+import boto3
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME"])
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args["JOB_NAME"], args)
 
-# Reading data from DynamoDB
-ddb_frame = glueContext.create_dynamic_frame.from_options(
-    connection_type="dynamodb",
-    connection_options={
-        "dynamodb.input.tableName": "tbes2004_web_rgto_crdl",
-        "dynamodb.throughput.read.percent": "0.5",
-        "dynamodb.splits": "100",
-    }
-)
+class GlueJob:
+    def __init__(self, args):
+        self.sc = SparkContext()
+        self.glueContext = GlueContext(self.sc)
+        self.spark = self.glueContext.spark_session
+        self.job = Job(self.glueContext)
+        self.job.init(args["JOB_NAME"], args)
+        self.s3_client = boto3.client("s3")
 
-# Writing data to S3
-glueContext.write_dynamic_frame.from_options(
-    frame=ddb_frame,
-    connection_type="s3",
-    connection_options={
-        "path": "s3://itau-corp-sor-sa-east-1-428345910379/glue/"
-    },
-    format="parquet",
-)
+    def read_from_dynamo(self, table_name):
+        return self.glueContext.create_dynamic_frame.from_options(
+            connection_type="dynamodb",
+            connection_options={
+                "dynamodb.input.tableName": table_name,
+                "dynamodb.throughput.read.percent": "1.0",
+                "dynamodb.splits": "1",
+            }
+        )
 
-job.commit()
+    def write_to_s3(self, frame, s3_path):
+        self.glueContext.write_dynamic_frame.from_options(
+            frame=frame,
+            connection_type="s3",
+            format="parquet",
+            connection_options={
+                "path": s3_path
+            }
+        )
+
+    def process(self, table_name, s3_path):
+        df_items = self.read_from_dynamo(table_name)
+        self.write_to_s3(df_items, s3_path)
+
+    def commit(self):
+        self.job.commit()
+
+
+def main():
+    args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+    glue_job = GlueJob(args)
+    table_name = "tbes2004_web_rgto_crdl"
+    s3_path = "s3://itau-corp-sor-sa-east-1-428345910379/glue/"
+    glue_job.process(table_name, s3_path)
+    glue_job.commit()
+
+
+if __name__ == "__main__":
+    main()
