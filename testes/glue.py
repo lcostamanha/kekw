9fe710d51653538c66c4ff7ca3fc255bb82d672c
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-07 19:14:42 -0300

    Update glue.py

--- testes/glue.py
+++ testes/glue.py
@@ -8,7 +8,6 @@ from awsglue.job import Job
 import boto3
 from datetime import datetime
 
-
 class GlueJob:
     def __init__(self, args):
         self.sc = SparkContext()
@@ -17,6 +16,12 @@ class GlueJob:
         self.job = Job(self.glueContext)
         self.job.init(args["JOB_NAME"], args)
         self.s3_client = boto3.client("s3")
+
+    def add_partition_cols(self, dynamic_frame):
+        current_date = datetime.now()
+        return dynamic_frame.withColumn("year", current_date.year)\
+                            .withColumn("month", current_date.month)\
+                            .withColumn("day", current_date.day)
 
     def read_from_dynamo(self, table_name):
         return self.glueContext.create_dynamic_frame.from_options(
@@ -33,31 +38,27 @@ class GlueJob:
             frame=frame,
             connection_type="s3",
             format="parquet",
-            connection_options={"path": s3_path}
+            connection_options={
+                "path": s3_path,
+                "partitionKeys": ["year", "month", "day"]
+            }
         )
 
     def process(self, table_name, s3_path):
         df_items = self.read_from_dynamo(table_name)
+        df_items = self.add_partition_cols(df_items)
         self.write_to_s3(df_items, s3_path)
 
     def commit(self):
         self.job.commit()
 
-
 def main():
     args = getResolvedOptions(sys.argv, ["JOB_NAME"])
     glue_job = GlueJob(args)
-    
     table_name = "tbes2004_web_rgto_crdl"
-
-    # Gerar caminho com estrutura de data
-    current_date = datetime.now()
-    s3_path = (f"s3://itau-corp-sor-sa-east-1-428345910379/glue/"
-               f"{current_date.year}/{current_date.month}/{current_date.day}/")
-    
+    s3_path = "s3://itau-corp-sor-sa-east-1-428345910379/glue/"
     glue_job.process(table_name, s3_path)
     glue_job.commit()
-
 
 if __name__ == "__main__":
     main()
