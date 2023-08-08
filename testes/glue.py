ec9c36dae04b66ce53cce82cd1c08e65e0996f50
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-08 12:28:46 -0300

    Update glue.py

--- testes/glue.py
+++ testes/glue.py
@@ -1,12 +1,10 @@
 import sys
-from awsglue.transforms import ApplyMapping
-from awsglue.utils import getResolvedOptions
-from awsglue.dynamicframe import DynamicFrame
+import boto3
 from pyspark.context import SparkContext
+from pyspark.sql.functions import current_date, date_format
 from awsglue.context import GlueContext
 from awsglue.job import Job
-import boto3
-from datetime import datetime
+from awsglue.utils import getResolvedOptions
 
 
 class GlueJob:
@@ -28,32 +26,30 @@ class GlueJob:
             }
         )
 
-    def add_partition_cols(self, dynamic_frame):
-        current_date = datetime.now()
-        df = dynamic_frame.toDF()
-        df = df.withColumn("year", current_date.year)\
-               .withColumn("month", current_date.month)\
-               .withColumn("day", current_date.day)
-        return DynamicFrame.fromDF(df, self.glueContext, "partitioned_frame")
-
-    def write_to_s3(self, frame, s3_path):
-        partitioned_frame = self.add_partition_cols(frame)
-        s3_path_with_partition = (
-            f"{s3_path}{datetime.now().year}/"
-            f"{datetime.now().month}/{datetime.now().day}/"
-        )
+    def write_to_s3(self, dynamic_frame, s3_bucket):
+        current_date_str = date_format(
+            current_date(), 'yyyy-MM-dd').alias('current_date')
+        df = dynamic_frame.toDF().withColumn(
+            'current_date', current_date_str)
+        df = df.withColumn('year', df.current_date.substr(1, 4))
+        df = df.withColumn('month', df.current_date.substr(6, 2))
+        df = df.withColumn('day', df.current_date.substr(9, 2))
+        s3_path = (f"s3://{s3_bucket}/data/"
+                   f"year={df.year}/month={df.month}/day={df.day}/")
+        df_partitioned = self.glueContext.create_dynamic_frame.fromDF(
+            df, self.glueContext, "df_partitioned")
         self.glueContext.write_dynamic_frame.from_options(
-            frame=partitioned_frame,
+            frame=df_partitioned,
             connection_type="s3",
             format="parquet",
             connection_options={
-                "path": s3_path_with_partition
+                "path": s3_path, "partitionKeys": ["year", "month", "day"]
             }
         )
 
-    def process(self, table_name, s3_path):
-        df_items = self.read_from_dynamo(table_name)
-        self.write_to_s3(df_items, s3_path)
+    def process(self, table_name, s3_bucket):
+        dynamic_frame = self.read_from_dynamo(table_name)
+        self.write_to_s3(dynamic_frame, s3_bucket)
 
     def commit(self):
         self.job.commit()
@@ -63,8 +59,8 @@ def main():
     args = getResolvedOptions(sys.argv, ["JOB_NAME"])
     glue_job = GlueJob(args)
     table_name = "tbes2004_web_rgto_crdl"
-    s3_path = "s3://itau-corp-sor-sa-east-1-428345910379/glue/"
-    glue_job.process(table_name, s3_path)
+    s3_bucket = "itau-corp-sor-sa-east-1-428345910379"
+    glue_job.process(table_name, s3_bucket)
     glue_job.commit()
 
 
