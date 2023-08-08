1a229c939db42f19821a36769f0bb507fa5e3614
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-08 16:18:02 -0300

    Update glue.py

--- testes/glue.py
+++ testes/glue.py
@@ -1,61 +1,42 @@
 import sys
-import boto3
+from datetime import datetime
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
 from awsglue.utils import getResolvedOptions
-from datetime import datetime
-
+from awsglue.job import Job
 
 class GlueJob:
     def __init__(self, args):
         self.sc = SparkContext()
         self.glueContext = GlueContext(self.sc)
-        self.spark = self.glueContext.spark_session
         self.job = Job(self.glueContext)
         self.job.init(args["JOB_NAME"], args)
-        self.s3_client = boto3.client("s3")
 
     def read_from_dynamo(self, table_name):
-        return self.glueContext.create_dynamic_frame.from_options(
-            "dynamodb", {
-                "dynamodb.input.tableName": table_name,
-                "dynamodb.throughput.read.percent": "1.0",
-                "dynamodb.splits": "10"
-            }
-        )
+        dynamo_frame = self.glueContext.create_dynamic_frame.from_catalog(
+            database="your_database_name", table_name=table_name)
+        return dynamo_frame.toDF()
 
-    def handle_incompatible_types(self, df):
-        json_rdd = df.toJSON()
-        return self.spark.read.json(json_rdd)
+    def write_to_s3(self, dataframe, s3_path):
+        dataframe.write.parquet(s3_path)
 
-    def write_to_s3(self, frame, s3_bucket):
-        df = frame.toDF()
-        df = self.handle_incompatible_types(df)
-        current_date = datetime.now()
-        year_val, month_val, day_val = (current_date.year, 
-                                        current_date.month, 
-                                        current_date.day)
-        path = (f"s3://{s3_bucket}/tb_fido/{year_val:04d}/"
-                f"{month_val:02d}/{day_val:02d}/")
-        df.write.parquet(path)
-
-    def process(self, table_name, s3_bucket):
-        frame = self.read_from_dynamo(table_name)
-        self.write_to_s3(frame, s3_bucket)
+    def process(self, table_name, s3_base_path):
+        df_items = self.read_from_dynamo(table_name)
+        current_date = datetime.now().strftime("%Y%m%d")
+        s3_path = f"{s3_base_path}/anomesdia={current_date}/"
+        self.write_to_s3(df_items, s3_path)
 
     def commit(self):
         self.job.commit()
-
 
 def main():
     args = getResolvedOptions(sys.argv, ["JOB_NAME"])
     glue_job = GlueJob(args)
     table_name = "tbes2004_web_rgto_crdl"
-    s3_bucket = "itau-corp-sor-sa-east-1-428345910379"
-    glue_job.process(table_name, s3_bucket)
+    s3_path = "s3://itau-corp-sor-sa-east-1-428345910379/tb_fido"
+    glue_job.process(table_name, s3_path)
     glue_job.commit()
-
 
 if __name__ == "__main__":
     main()
