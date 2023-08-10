b689665f7792d58629c3110b0c0f8be84b8a04bb
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-10 11:13:46 -0300

    Update glue.py

--- testes/glue.py
+++ testes/glue.py
@@ -1,25 +1,26 @@
 import sys
+from datetime import datetime
 from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from pyspark.context import SparkContext
 import boto3
-from datetime import datetime
-
 
 class GlueJob:
     def __init__(self, args):
         self.sc = SparkContext()
         self.glueContext = GlueContext(self.sc)
-        self.spark = self.glueContext.spark_session
         self.job = Job(self.glueContext)
         self.job.init(args["JOB_NAME"], args)
-        self.glue_client = boto3.client("glue")
 
     def read_from_dynamo(self, table_name):
-        return self.glueContext.create_dynamic_frame.from_catalog(
-            database="your_database_name",
-            table_name=table_name
+        return self.glueContext.create_dynamic_frame.from_options(
+            connection_type="dynamodb",
+            connection_options={
+                "dynamodb.input.tableName": table_name,
+                "dynamodb.throughput.read.percent": "1.0",
+                "dynamodb.splits": "1",
+            }
         )
 
     def write_to_s3(self, frame, s3_path):
@@ -27,50 +28,26 @@ class GlueJob:
             frame=frame,
             connection_type="s3",
             format="parquet",
-            connection_options={"path": s3_path}
-        )
-
-    def create_partition(self, database_name, table_name, s3_path,
-                         partition_values):
-        self.glue_client.create_partition(
-            DatabaseName=database_name,
-            TableName=table_name,
-            PartitionInput={
-                "Values": partition_values,
-                "StorageDescriptor": {
-                    "Location": s3_path,
-                    "InputFormat": "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat",
-                    "OutputFormat": "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat",
-                    "SerdeInfo": {
-                        "SerializationLibrary": "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
-                    }
-                }
+            connection_options={
+                "path": s3_path
             }
         )
 
-    def process(self, table_name, s3_base_path, database_name,
-                athena_table_name):
+    def process(self, table_name, s3_path):
         df_items = self.read_from_dynamo(table_name)
-        current_date = datetime.now().strftime("%Y%m%d")
-        s3_path = f"{s3_base_path}anomesdia={current_date}/"
         self.write_to_s3(df_items, s3_path)
-        self.create_partition(database_name, athena_table_name, s3_path,
-                              [current_date])
 
     def commit(self):
         self.job.commit()
-
 
 def main():
     args = getResolvedOptions(sys.argv, ["JOB_NAME"])
     glue_job = GlueJob(args)
     table_name = "tbes2004_web_rgto_crdl"
-    s3_base_path = "s3://itau-corp-sor-sa-east-1-428345910379/tb_fido/"
-    database_name = "db_corp_identificacaoeautenticacaodeclientes_customeriam_sor_01"
-    athena_table_name = "tb_fido"
-    glue_job.process(table_name, s3_base_path, database_name, athena_table_name)
+    s3_path = "s3://itau-corp-sor-sa-east-1-428345910379/tb_fido/anomesdia=" + \
+        datetime.now().strftime("%Y%m%d") + "/"
+    glue_job.process(table_name, s3_path)
     glue_job.commit()
-
 
 if __name__ == "__main__":
     main()
