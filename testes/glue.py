ae6625b5c7a1bacbb446bca08eedccb8421cc1bd
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-17 13:37:45 -0300

    Update glue.py

--- testes/glue.py
+++ testes/glue.py
@@ -32,36 +32,67 @@ class GlueJob:
             connection_options={"path": s3_path}
         )
 
-    def create_partition(self, database_name, table_name, s3_path, partition_value):
-        self.glue_client.create_partition(
-            DatabaseName=database_name,
-            TableName=table_name,
-            PartitionInput={
-                "Values": [partition_value],
-                "StorageDescriptor": {
-                    "Location": s3_path
+    def create_partition(self, database_name, table_name, s3_path, partition_value,
+                         table_data):
+        try:
+            self.glue_client.create_partition(
+                DatabaseName=database_name,
+                TableName=table_name,
+                CatalogId="773683090477",
+                PartitionInput={
+                    "Values": [partition_value],
+                    "StorageDescriptor": {
+                        "Location": s3_path,
+                        "InputFormat": table_data["input_format"],
+                        "OutputFormat": table_data["output_format"],
+                        "SerdeInfo": table_data["serde_info"]
+                    }
                 }
-            }
-        )
+            )
+        except Exception as e:
+            print("Error create_partition", e)
 
-    def process(self, table_name, s3_base_path, database_name, glue_table_name):
+    def get_table_schema(self, CatalogId, database_name, table_name):
+        try:
+            response = self.glue_client.get_table(
+                CatalogId=CatalogId,
+                DatabaseName=database_name,
+                Name=table_name
+            )
+        except Exception as error:
+            print("Exception while fetching table info")
+            sys.exit(-1)
+
+        table_data = {}
+        table_data['input_format'] = response['Table']['StorageDescriptor']['InputFormat']
+        table_data['output_format'] = response['Table']['StorageDescriptor']['OutputFormat']
+        table_data['table_location'] = response['Table']['StorageDescriptor']['Location']
+        table_data['serde_info'] = response['Table']['StorageDescriptor']['SerdeInfo']
+        table_data['partition_keys'] = response['Table']['PartitionKeys']
+        return table_data
+
+    def process(self, table_name, s3_base_path, database_name, glue_table_name,
+                CatalogId):
         dynamo_frame = self.read_from_dynamo(table_name)
         current_date = datetime.now().strftime("%Y%m%d")
         s3_path = f"{s3_base_path}/anomesdia={current_date}/"
+        table_data = self.get_table_schema(CatalogId, database_name, glue_table_name)
         self.write_to_s3(dynamo_frame, s3_path)
-        self.create_partition(database_name, glue_table_name, s3_path, current_date)
+        self.create_partition(database_name, glue_table_name, s3_path, current_date,
+                              table_data)
 
     def commit(self):
         self.job.commit()
 
 def main():
-    args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+    args = getResolvedOptions(sys.argv, ["JOB_NAME", "CatalogIdControl", "BKT_DEST"])
+    CatalogId = args["CatalogIdControl"]
     glue_job = GlueJob(args)
     table_name = "tbes2004_web_rgto_crdl"
-    s3_path = "s3://itau-corp-sor-sa-east-1-428345910379/tb_fido"
-    database_name = "db_corp_identificacaoeautenticacaodeclientes_customeriam_sor_01"
+    s3_path = "s3://{BKT}/tb_fido".format(BKT=args["BKT_DEST"])
+    database_name = "db_source_identificacaoeautenticacaodeclientes_customeriam_sor_01"
     glue_table_name = "tb_fido"
-    glue_job.process(table_name, s3_path, database_name, glue_table_name)
+    glue_job.process(table_name, s3_path, database_name, glue_table_name, CatalogId)
     glue_job.commit()
 
 if __name__ == "__main__":
