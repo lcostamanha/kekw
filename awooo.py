49dadcc8bd414b7c7ee77579141858dd611a4728
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-24 14:33:45 -0300

    Update awooo.py

--- awooo.py
+++ awooo.py
@@ -1,26 +1,15 @@
 import unittest
+from main import process_data
 
-class TestGlueJobIntegration(unittest.TestCase):
-    
-    def test_glue_job_process(self):
-        # Dummy args
-        args = {
-            "JOB_NAME": "test_job",
-            "CatalogIdControl": "dummy_catalog_id",
-            "BKT_DEST": "dummy_bucket"
-        }
+class TestMainFunctionality(unittest.TestCase):
 
-        try:
-            glue_job = GlueJob(args)
-            table_name = "dummy_table_name"
-            s3_base_path = "s3://dummy_path"
-            database_name = "dummy_database_name"
-            glue_table_name = "dummy_glue_table_name"
-            CatalogId = "dummy_catalog_id"
-            glue_job.process(table_name, s3_base_path, database_name, glue_table_name, CatalogId)
-            glue_job.commit()
-        except Exception as e:
-            self.fail(f"Test failed with exception: {e}")
+    def test_process_data(self):
+        input_data = "some input"
+        expected_output = "expected output"
+        
+        result = process_data(input_data)
+        
+        self.assertEqual(result, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
