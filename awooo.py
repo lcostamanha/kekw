33e09fc1e50ea16d05d0a68475e6414c4b81d296
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-28 11:44:51 -0300

    Update awooo.py

--- awooo.py
+++ awooo.py
@@ -1,6 +1,13 @@
-TaskDefinition:
-  UpdateReplacePolicy: Retain
-  Type: "AWS::ECS::TaskDefinition"
-  Properties:
-    ExecutionRoleArn: !Ref TaskExecutionRole
-    TaskRoleArn: !Ref TaskDefinitionRole
+Parameters:
+  ExistingExecutionRoleArn:
+    Type: String
+  ExistingTaskRoleArn:
+    Type: String
+
+Resources:
+  TaskDefinition:
+    Type: "AWS::ECS::TaskDefinition"
+    Properties:
+      ExecutionRoleArn: !Ref ExistingExecutionRoleArn
+      TaskRoleArn: !Ref ExistingTaskRoleArn
+      # Outras propriedades aqui
