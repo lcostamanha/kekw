edae44a5dbf6effbcd97052c682ac6dd7b67e794
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-08-28 11:23:56 -0300

    Update awooo.py

--- awooo.py
+++ awooo.py
@@ -1,15 +1,6 @@
-import unittest
-from main import process_data
-
-class TestMainFunctionality(unittest.TestCase):
-
-    def test_process_data(self):
-        input_data = "some input"
-        expected_output = "expected output"
-        
-        result = process_data(input_data)
-        
-        self.assertEqual(result, expected_output)
-
-if __name__ == '__main__':
-    unittest.main()
+TaskDefinition:
+  UpdateReplacePolicy: Retain
+  Type: "AWS::ECS::TaskDefinition"
+  Properties:
+    ExecutionRoleArn: !Ref TaskExecutionRole
+    TaskRoleArn: !Ref TaskDefinitionRole
