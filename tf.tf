e44eea0a4435b467a11f551846bc1790996a2e1f
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-09-11 09:59:10 -0300

    Update tf.tf

--- tf.tf
+++ tf.tf
@@ -1,1 +1,45 @@
+configurations = [
+  {
+    applications = [
+      {
+        name = "webauthnbff",
+        parameters = [
+          {
+            name = "gw.address"
+            description = ""
+            type = "String"
+            value = "https://x4azcevw82.execute-api.sa-east-1.amazonaws.com/dev"
+            isSensitive = false
+          },
+          {
+            name = "http.maxconnections"
+            description = ""
+            type = "String"
+            value = "5"
+            isSensitive = false
+          }
+        ]
+      },
+      {
+        name = "fido",
+        parameters = [
+          {
+            name = "alguma_configuracao"
+            description = ""
+            type = "String"
+            value = "algum_valor"
+            isSensitive = false
+          },
+          {
+            name = "outra_configuracao"
+            description = ""
+            type = "String"
+            value = "outro_valor"
+            isSensitive = false
+          }
+        ]
+      }
+    ]
+  }
+]
 
