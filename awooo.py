1c5f9af0855a32718ad40617a0558aef185bbbac
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-09-15 15:43:14 -0300

    Update awooo.py

--- awooo.py
+++ awooo.py
@@ -1,8 +1,7 @@
 SELECT 
-  element.description
+  element.element.description
 FROM 
   "db_corp_identificacaoeautenticacaodeclientes_customeriam_sor_01"."tb_fido",
   UNNEST(txt_objt_chav_pubi.nom_idef_mtdo.device_properties) AS t(element)
 WHERE 
-  anomesdia = 20230914 
-  AND length(nom_usua_cada_crdl) > 9;
+  anomesdia = 20230914;
