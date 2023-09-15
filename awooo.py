53001d8ca455b906af9cb3282374121145ea8c89
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-09-15 15:34:29 -0300

    Update awooo.py

--- awooo.py
+++ awooo.py
@@ -1,10 +1,8 @@
-SELECT DISTINCT 
-    nom_usua_cada_crdl as original, 
-    substring(nom_usua_cada_crdl, 1, 4) as agencia, 
-    substring(nom_usua_cada_crdl, 5, 5) as conta,
-    substring(nom_usua_cada_crdl, 10, 1) as dac 
-FROM "db_corp_identificacaoeautenticacaodeclientes_customeriam_sor_01"."tb_fido" 
+SELECT 
+  element.description
+FROM 
+  "db_corp_identificacaoeautenticacaodeclientes_customeriam_sor_01"."tb_fido",
+  UNNEST(nom_idef_mtdo.device_properties) AS t(element)
 WHERE 
-    anomesdia = 20230914 
-    AND length(nom_usua_cada_crdl) > 9
-    AND SUBSTRING(txt_objt_chav_pubi.txt_objt_idef_chav.txt_idef_chav, LENGTH(txt_objt_chav_pubi.txt_objt_idef_chav.txt_idef_chav) - 3, 4) != 'AAAA';
+  anomesdia = 20230914 
+  AND length(nom_usua_cada_crdl) > 9;
