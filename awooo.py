a9b006568b81cfe3492da6a91ce9446d75677a88
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-09-15 15:26:27 -0300

    Update awooo.py

--- awooo.py
+++ awooo.py
@@ -1,13 +1,10 @@
-Parameters:
-  ExistingExecutionRoleArn:
-    Type: String
-  ExistingTaskRoleArn:
-    Type: String
-
-Resources:
-  TaskDefinition:
-    Type: "AWS::ECS::TaskDefinition"
-    Properties:
-      ExecutionRoleArn: !Ref ExistingExecutionRoleArn
-      TaskRoleArn: !Ref ExistingTaskRoleArn
-      # Outras propriedades aqui
+SELECT DISTINCT 
+    nom_usua_cada_crdl as original, 
+    substring(nom_usua_cada_crdl, 1, 4) as agencia, 
+    substring(nom_usua_cada_crdl, 5, 5) as conta,
+    substring(nom_usua_cada_crdl, 10, 1) as dac 
+FROM "db_corp_identificacaoeautenticacaodeclientes_customeriam_sor_01"."tb_fido" 
+WHERE 
+    anomesdia = 20230914 
+    AND length(nom_usua_cada_crdl) > 9
+    AND SUBSTRING(txt_objt_chav_pubi.txt_objt_idef_chav.txt_idef_chav, LENGTH(txt_objt_chav_pubi.txt_objt_idef_chav.txt_idef_chav) - 3, 4) != 'AAAA';
