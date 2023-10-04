42ea6c5fe4aebab179e8a2daeacfc04beb3d575d
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-10-04 17:27:22 -0300

    Update tf.tf

--- tf.tf
+++ tf.tf
@@ -1,8 +1,29 @@
-resource "aws_secretsmanager_secret" "example_secret" {
-  name = "example_secret"
-}
+Governança em Observabilidade: Modelo de Engajamento com Ponto Focal
 
-resource "aws_secretsmanager_secret_version" "example_secret_version" {
-  secret_id     = aws_secretsmanager_secret.example_secret.id
-  secret_string = "my_secret_value"
-}
+
+Fase 1: Demonstração e Acompanhamento Inicial
+Objetivo: Mostrar como implementar práticas de observabilidade de acordo com o DoD.
+Responsáveis: Equipe de governança.
+Ação: Realizar uma demonstração prática e configurar os primeiros elementos de observabilidade.
+Acompanhamento: O ponto focal da squad acompanha todo o processo, mas não executa.
+
+
+Fase 2: Co-Implementação
+Objetivo: Capacitar o ponto focal para que possa conduzir parte da implementação.
+Responsáveis: Equipe de governança e ponto focal.
+Ação: Ambas as partes trabalham juntas na implementação de novos aspectos de observabilidade.
+Acompanhamento: O ponto focal começa a assumir algumas responsabilidades sob supervisão.
+
+
+Fase 3: Autonomia do Ponto Focal
+Objetivo: O ponto focal deve ser capaz de implementar e manter práticas de observabilidade autonomamente.
+Responsáveis: Ponto focal.
+Ação: O ponto focal implementa novos aspectos ou faz ajustes nos existentes.
+Acompanhamento: A equipe de governança faz revisões periódicas para garantir a conformidade com o DoD.
+Indicadores e Métricas
+Definir KPIs específicos para cada fase.
+Usar dados da autoavaliação para medir progresso.
+Documentação e Conformidade
+Manter documentação atualizada para cada fase.
+Realizar auditorias para garantir que as práticas estão alinhadas com o DoD.
+Esse esboço faz sentido para você? Algum ajuste ou ponto adicional que você gostaria de considerar?
