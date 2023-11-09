809a02dbb35b4b87d81a41ce8c6581c363fe5560
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-11-09 10:57:48 -0300

    Update tf.tf

--- tf.tf
+++ tf.tf
@@ -7,13 +7,13 @@ TargetGroupNew:
     VpcId: !Ref VPCIDNEW
     HealthyThresholdCount: 10
     UnhealthyThresholdCount: 10
+    HealthCheckProtocol: HTTP
+    HealthCheckPath: '/' # Ou o caminho específico para a verificação de saúde da sua aplicação
+    HealthCheckPort: 'traffic-port' # Utiliza a mesma porta de tráfego para o health check, ou especifique uma porta.
+    HealthCheckEnabled: true # Certifique-se de que a verificação de saúde está habilitada.
+    HealthCheckIntervalSeconds: 30 # O intervalo entre as verificações de saúde.
+    HealthCheckTimeoutSeconds: 5 # O número de segundos durante os quais nenhuma resposta significa uma falha de health check.
     TargetGroupAttributes:
       - Key: deregistration_delay.timeout_seconds
         Value: '5'
     TargetType: ip
-    HealthCheckProtocol: HTTP
-    HealthCheckPort: 'traffic-port' # Utilize o mesmo porto que o tráfego está usando ou especifique um porto específico
-    HealthCheckEnabled: true
-    HealthCheckPath: '/' # O caminho que o load balancer deve chamar para realizar o health check
-    Matcher:
-      HttpCode: '200' # Os códigos de resposta para determinar um servidor saudável
