0a3a29a473e87adfe7fb54f380d06ddb8e03ed2e
Lucas Costamanha <51381836+lcostamanha@users.noreply.github.com>
2023-11-09 10:07:42 -0300

    Update tf.tf

--- tf.tf
+++ tf.tf
@@ -1,29 +1,19 @@
-Governança em Observabilidade: Modelo de Engajamento com Ponto Focal
-
-
-Fase 1: Demonstração e Acompanhamento Inicial
-Objetivo: Mostrar como implementar práticas de observabilidade de acordo com o DoD.
-Responsáveis: Equipe de governança.
-Ação: Realizar uma demonstração prática e configurar os primeiros elementos de observabilidade.
-Acompanhamento: O ponto focal da squad acompanha todo o processo, mas não executa.
-
-
-Fase 2: Co-Implementação
-Objetivo: Capacitar o ponto focal para que possa conduzir parte da implementação.
-Responsáveis: Equipe de governança e ponto focal.
-Ação: Ambas as partes trabalham juntas na implementação de novos aspectos de observabilidade.
-Acompanhamento: O ponto focal começa a assumir algumas responsabilidades sob supervisão.
-
-
-Fase 3: Autonomia do Ponto Focal
-Objetivo: O ponto focal deve ser capaz de implementar e manter práticas de observabilidade autonomamente.
-Responsáveis: Ponto focal.
-Ação: O ponto focal implementa novos aspectos ou faz ajustes nos existentes.
-Acompanhamento: A equipe de governança faz revisões periódicas para garantir a conformidade com o DoD.
-Indicadores e Métricas
-Definir KPIs específicos para cada fase.
-Usar dados da autoavaliação para medir progresso.
-Documentação e Conformidade
-Manter documentação atualizada para cada fase.
-Realizar auditorias para garantir que as práticas estão alinhadas com o DoD.
-Esse esboço faz sentido para você? Algum ajuste ou ponto adicional que você gostaria de considerar?
+TargetGroupNew:
+  Type: AWS::ElasticLoadBalancingV2::TargetGroup
+  DependsOn: LoadBalancerNew
+  Properties:
+    Port: !Ref ExposedPortInDockerfile
+    Protocol: TCP
+    VpcId: !Ref VPCIDNEW
+    HealthyThresholdCount: 10
+    UnhealthyThresholdCount: 10
+    TargetGroupAttributes:
+      - Key: deregistration_delay.timeout_seconds
+        Value: '5'
+    TargetType: ip
+    HealthCheckProtocol: HTTP
+    HealthCheckPort: 'traffic-port' # Utilize o mesmo porto que o tráfego está usando ou especifique um porto específico
+    HealthCheckEnabled: true
+    HealthCheckPath: '/' # O caminho que o load balancer deve chamar para realizar o health check
+    Matcher:
+      HttpCode: '200' # Os códigos de resposta para determinar um servidor saudável
