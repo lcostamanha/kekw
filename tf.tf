3701e945dd578e35e67af16f09a652334084e97b
lcostamanha <lcostamanha@gmail.com>
2024-03-14 17:56:29 -0300

    Update tf.tf

--- tf.tf
+++ tf.tf
@@ -1,19 +1,51 @@
-TargetGroupNew:
-  Type: AWS::ElasticLoadBalancingV2::TargetGroup
-  DependsOn: LoadBalancerNew
-  Properties:
-    Port: !Ref ExposedPortInDockerfile
-    Protocol: TCP
-    VpcId: !Ref VPCIDNEW
-    HealthyThresholdCount: 10
-    UnhealthyThresholdCount: 10
-    HealthCheckProtocol: HTTP
-    HealthCheckPath: '/' # Ou o caminho específico para a verificação de saúde da sua aplicação
-    HealthCheckPort: 'traffic-port' # Utiliza a mesma porta de tráfego para o health check, ou especifique uma porta.
-    HealthCheckEnabled: true # Certifique-se de que a verificação de saúde está habilitada.
-    HealthCheckIntervalSeconds: 30 # O intervalo entre as verificações de saúde.
-    HealthCheckTimeoutSeconds: 5 # O número de segundos durante os quais nenhuma resposta significa uma falha de health check.
-    TargetGroupAttributes:
-      - Key: deregistration_delay.timeout_seconds
-        Value: '5'
-    TargetType: ip
+resource "aws_lb" "nlb" {
+  name               = "nlb-jdk11-newvpc-${var.micro_service_name}"
+  internal           = true
+  load_balancer_type = "network"
+  subnets            = [var.private_subnet_one_new, var.private_subnet_two_new, var.private_subnet_three_new]
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = var.listener_container_port
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08" # Customize based on requirements
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+  certificate_arn = var.certificate_arn
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "tg-${var.micro_service_name}"
+  port     = var.listener_container_port
+  protocol = "TCP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_route53_record" "dns_record" {
+  zone_id = var.hosted_zone_id
+  name    = var.dns_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.nlb.dns_name
+    zone_id                = aws_lb.nlb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+variable "micro_service_name" {}
+variable "private_subnet_one_new" {}
+variable "private_subnet_two_new" {}
+variable "private_subnet_three_new" {}
+variable "listener_container_port" {}
+variable "certificate_arn" {}
+variable "vpc_id" {}
+variable "hosted_zone_id" {}
+variable "dns_name" {}
